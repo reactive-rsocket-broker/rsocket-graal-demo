6704fe80c62bd552835a0014b442ec439260885c
linux_china <libing.chen@gmail.com>
2020-12-18 12:21:52 -0800

    adjust to Reactive type

--- src/main/java/org/mvnsearch/rsocket/graal/RSocketController.java
+++ src/main/java/org/mvnsearch/rsocket/graal/RSocketController.java
@@ -2,12 +2,13 @@ package org.mvnsearch.rsocket.graal;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
+import reactor.core.publisher.Mono;
 
 @Controller
 public class RSocketController {
 
     @MessageMapping("request-response")
-    public String requestResponse(String name) {
-        return "Hello " + name;
+    public Mono<String> requestResponse(String name) {
+        return Mono.just("Hello " + name);
     }
 }
