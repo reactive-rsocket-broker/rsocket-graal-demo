7c6ce8bd5e6153e95975130eef45b720cee1c2e2
linux_china <libing.chen@gmail.com>
2021-03-11 19:16:44 -0800

    AppUser

---
+++ src/main/java/org/mvnsearch/rsocket/graal/AppUser.java
@@ -0,0 +1,22 @@
+package org.mvnsearch.rsocket.graal;
+
+public class AppUser {
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
